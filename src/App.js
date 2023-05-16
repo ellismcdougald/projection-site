3c60d8c7457653895d4a37b894a15c1f2e56cf5d
Ellis McDougald <ecmcdougald@gmail.com>
2023-05-15 19:53:17 -0600

    Sets up app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PlayerSelect from "./components/playerSelect";
+import { useState } from "react";
 
 function App() {
+  const [selectedPlayerIndex, setSelectedPlayerIndex] = useState(null);
+
+  function toggleSelectedPlayerIndex(playerIndex) {
+    console.log("test");
+    setSelectedPlayerIndex(playerIndex);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlayerSelect
+        selectedPlayerIndex={selectedPlayerIndex}
+        toggleSelectedPlayerIndex={toggleSelectedPlayerIndex}
+      />
     </div>
   );
 }
