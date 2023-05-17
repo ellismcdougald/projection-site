407c3b45972067f15ded01c9d9acc8a6de4896a7
Ellis McDougald <ecmcdougald@gmail.com>
2023-05-16 19:05:21 -0600

    Pulls data from csv

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import "./App.css";
 import PlayerSelect from "./components/playerSelect";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const PLAYERS = [
+  {
+    name: "Connor McDavid",
+    position: "C",
+    season: "2021-22",
+    id: 1,
+  },
+  {
+    name: "Leon Draisaitl",
+    position: "C",
+    season: "2021-22",
+    id: 2,
+  },
+];
 
 function App() {
-  const [selectedPlayerIndex, setSelectedPlayerIndex] = useState(null);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [players, setPlayers] = useState(null);
 
-  function toggleSelectedPlayerIndex(playerIndex) {
-    console.log("test");
-    setSelectedPlayerIndex(playerIndex);
+  function toggleSelectedPlayer(playerIndex) {
+    setSelectedPlayer(playerIndex);
   }
+
+  useEffect(() => {
+    fetch("data.csv").then((response) =>
+      response.text().then((text) =>
+        setPlayers(
+          text
+            .split("\n")
+            .map((i) => i.split(","))
+            .map((i) => {
+              return { name: i[0], position: i[1], season: i[2], id: i[3] };
+            })
+        )
+      )
+    );
+  }, [players]);
 
   return (
     <div className="App">
       <PlayerSelect
-        selectedPlayerIndex={selectedPlayerIndex}
-        toggleSelectedPlayerIndex={toggleSelectedPlayerIndex}
+        options={players}
+        selectedPlayer={selectedPlayer}
+        toggleSelectedPlayer={toggleSelectedPlayer}
       />
     </div>
   );
