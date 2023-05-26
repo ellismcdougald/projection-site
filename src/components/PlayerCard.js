0652d5f1b87601b0e63ea1d957f64b6b68e58d70
Ellis McDougald <ecmcdougald@gmail.com>
2023-05-25 20:56:51 -0600

    Adds z-score bar chart

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -7,13 +7,24 @@ import {
   CategoryScale,
   LinearScale,
   PointElement,
+  BarElement,
+  Tooltip,
 } from "chart.js";
+import { Bar } from "react-chartjs-2";
+
 import ComparablesContainer from "./ComparablesContainer";
 
-ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Tooltip,
+  BarElement
+);
 
 export default function PlayerCard(props) {
-  const data = {
+  const lineData = {
     labels: ["20", "21", "22", "23", "24", "25", "26", "27"],
     datasets: [
       {
@@ -28,7 +39,7 @@ export default function PlayerCard(props) {
       },
     ],
   };
-  const options = {
+  const lineOptions = {
     plugins: {
       title: {
         display: true,
@@ -54,6 +65,78 @@ export default function PlayerCard(props) {
     },
   };
 
+  const barOptions = {
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    scales: {
+      x: {
+        suggestedMax: 3,
+        suggestedMin: -3,
+      },
+      y: {
+        ticks: {
+          autoSkip: false,
+        },
+      },
+    },
+  };
+  const labels = [
+    // bio
+    "Height",
+    "Weight",
+    "Draft Pos.",
+    // scoring
+    "Goals",
+    "Shots",
+    "Sh%",
+    "xGoals",
+    "Scoring Chances",
+    "High-Danger Shots",
+    "Rebounds Created",
+    // playmaking
+    "First Assists",
+    "Second Assists",
+    "Rush Attempts",
+    "Penalties Drawn",
+    // usage
+    "TOI",
+    "TOI/GP",
+  ];
+
+  const barData = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [
+          props.player["z_height"],
+          props.player["z_weight"],
+          props.player["z_draft_position"],
+          props.player["z_goals/60"],
+          props.player["z_shots/60"],
+          props.player["z_sh%"],
+          props.player["z_ixg/60"],
+          props.player["z_iscf/60"],
+          props.player["z_ihdcf/60"],
+          props.player["z_rebounds_created/60"],
+          props.player["z_first_assists/60"],
+          props.player["z_second_assists/60"],
+          props.player["z_rush_attempts/60"],
+          props.player["z_penalties_drawn/60"],
+          props.player["z_toi"],
+          props.player["z_toi/gp"],
+        ],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
   return (
     <div id={styles.container}>
       {props.player && (
@@ -70,7 +153,10 @@ export default function PlayerCard(props) {
             </div>
           </div>
           <div id={styles.projectionChartContainer}>
-            <Line data={data} options={options}></Line>
+            <Line data={lineData} options={lineOptions}></Line>
+          </div>
+          <div id={styles.statsContainer}>
+            <Bar data={barData} options={barOptions} />
           </div>
         </div>
       )}
