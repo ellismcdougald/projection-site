be0541a3e9af557f25e19a998df44f91ceea3282
Ellis McDougald <ecmcdougald@gmail.com>
2023-05-26 22:44:28 -0600

    Moves charts to standalone components

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,146 +1,15 @@
 import React from "react";
 import styles from "../styles/PlayerCard.module.css";
-import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  BarElement,
-  Tooltip,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-
+import TrajectoryChart from "./TrajectoryChart";
 import ComparablesContainer from "./ComparablesContainer";
-
-ChartJS.register(
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Tooltip,
-  BarElement
-);
+import SkillsChart from "./SkillsChart";
 
 export default function PlayerCard(props) {
-  const lineData = {
-    labels: ["20", "21", "22", "23", "24", "25", "26", "27"],
-    datasets: [
-      {
-        labels: "Projection",
-        data: [
-          1.95, 2.46, 3.84, 3.03928268, 2.9608289127, 2.9737694739,
-          3.1355211395, 3.3008620495,
-        ],
-        backgroundColor: "black",
-        borderColor: "black",
-        pointBorderColor: "black",
-      },
-    ],
-  };
-  const lineOptions = {
-    plugins: {
-      title: {
-        display: true,
-        text: "Custom Chart Title",
-      },
-      legend: false,
-    },
-    scales: {
-      y: {
-        title: {
-          display: false,
-          text: "PTS / 60",
-        },
-        min: 0,
-        max: 6,
-      },
-      x: {
-        title: {
-          display: false,
-          text: "Season",
-        },
-      },
-    },
-  };
-
-  const barOptions = {
-    indexAxis: "y",
-    elements: {
-      bar: {
-        borderWidth: 2,
-      },
-    },
-    responsive: true,
-    scales: {
-      x: {
-        suggestedMax: 3,
-        suggestedMin: -3,
-      },
-      y: {
-        ticks: {
-          autoSkip: false,
-        },
-      },
-    },
-  };
-  const labels = [
-    // bio
-    "Height",
-    "Weight",
-    "Draft Pos.",
-    // scoring
-    "Goals",
-    "Shots",
-    "Sh%",
-    "xGoals",
-    "Scoring Chances",
-    "High-Danger Shots",
-    "Rebounds Created",
-    // playmaking
-    "First Assists",
-    "Second Assists",
-    "Rush Attempts",
-    "Penalties Drawn",
-    // usage
-    "TOI",
-    "TOI/GP",
-  ];
-
-  const barData = {
-    labels,
-    datasets: [
-      {
-        label: "Dataset 1",
-        data: [
-          props.player["z_height"],
-          props.player["z_weight"],
-          props.player["z_draft_position"],
-          props.player["z_goals/60"],
-          props.player["z_shots/60"],
-          props.player["z_sh%"],
-          props.player["z_ixg/60"],
-          props.player["z_iscf/60"],
-          props.player["z_ihdcf/60"],
-          props.player["z_rebounds_created/60"],
-          props.player["z_first_assists/60"],
-          props.player["z_second_assists/60"],
-          props.player["z_rush_attempts/60"],
-          props.player["z_penalties_drawn/60"],
-          props.player["z_toi"],
-          props.player["z_toi/gp"],
-        ],
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-    ],
-  };
-
   return (
     <div id={styles.container}>
       {props.player && (
         <div id={styles.cardContainer}>
+          {/* {make player bio a component} */}
           <div id={styles.playerBio}>
             <h1 className={styles.headerText}>{props.player.player}</h1>
             <h2 className={styles.headerText} id={styles.positionSeasonText}>
@@ -152,12 +21,11 @@ export default function PlayerCard(props) {
               </h3>
             </div>
           </div>
-          <div id={styles.projectionChartContainer}>
-            <Line data={lineData} options={lineOptions}></Line>
-          </div>
-          <div id={styles.statsContainer}>
-            <Bar data={barData} options={barOptions} />
-          </div>
+          <TrajectoryChart
+            data={[1, 2, 3, 3, 3, 3, 3, 3]}
+            labels={["20", "21", "22", "23", "24", "25", "26", "27"]}
+          />
+          <SkillsChart player={props.player} />
         </div>
       )}
     </div>
