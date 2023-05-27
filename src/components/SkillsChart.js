3260216a451f45c52224d5b04a3193cdb353c313
Ellis McDougald <ecmcdougald@gmail.com>
2023-05-26 22:45:00 -0600

    Moves charts to standalone components

---
+++ src/components/SkillsChart.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Tooltip,
+} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, Tooltip, BarElement);
+
+export default function SkillsChart(props) {
+  const options = {
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    scales: {
+      x: {
+        suggestedMax: 3,
+        suggestedMin: -3,
+      },
+      y: {
+        ticks: {
+          autoSkip: false,
+        },
+      },
+    },
+  };
+  const labels = [
+    // bio
+    "Height",
+    "Weight",
+    "Draft Pos.",
+    // scoring
+    "Goals",
+    "Shots",
+    "Sh%",
+    "xGoals",
+    "Scoring Chances",
+    "High-Danger Shots",
+    "Rebounds Created",
+    // playmaking
+    "First Assists",
+    "Second Assists",
+    "Rush Attempts",
+    "Penalties Drawn",
+    // usage
+    "TOI",
+    "TOI/GP",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [
+          props.player["z_height"],
+          props.player["z_weight"],
+          props.player["z_draft_position"],
+          props.player["z_goals/60"],
+          props.player["z_shots/60"],
+          props.player["z_sh%"],
+          props.player["z_ixg/60"],
+          props.player["z_iscf/60"],
+          props.player["z_ihdcf/60"],
+          props.player["z_rebounds_created/60"],
+          props.player["z_first_assists/60"],
+          props.player["z_second_assists/60"],
+          props.player["z_rush_attempts/60"],
+          props.player["z_penalties_drawn/60"],
+          props.player["z_toi"],
+          props.player["z_toi/gp"],
+        ],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+  return (
+    <div>
+      <Bar data={data} options={options} />
+    </div>
+  );
+}
---
+++ src/components/TrajectoryChart.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Tooltip,
+} from "chart.js";
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Tooltip
+);
+
+export default function TrajectoryChart(props) {
+  const data = {
+    labels: props.labels,
+    datasets: [
+      {
+        labels: "Projection",
+        data: props.data,
+        backgroundColor: "black",
+        borderColor: "black",
+        pointBorderColor: "black",
+      },
+    ],
+  };
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: "Custom Chart Title",
+      },
+      legend: false,
+    },
+    scales: {
+      y: {
+        title: {
+          display: false,
+          text: "PTS / 60",
+        },
+        min: 0,
+        max: 6,
+      },
+      x: {
+        title: {
+          display: false,
+          text: "Season",
+        },
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Line data={data} options={options}></Line>
+    </div>
+  );
+}
