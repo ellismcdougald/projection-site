8f9cd12cbe0913446cf6f3a9961a492dfcf94a16
Ellis McDougald <ecmcdougald@gmail.com>
2023-05-26 22:50:27 -0600

    Creates standalone player bio component

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -3,24 +3,19 @@ import styles from "../styles/PlayerCard.module.css";
 import TrajectoryChart from "./TrajectoryChart";
 import ComparablesContainer from "./ComparablesContainer";
 import SkillsChart from "./SkillsChart";
+import PlayerBio from "./PlayerBio";
 
 export default function PlayerCard(props) {
   return (
     <div id={styles.container}>
       {props.player && (
         <div id={styles.cardContainer}>
-          {/* {make player bio a component} */}
-          <div id={styles.playerBio}>
-            <h1 className={styles.headerText}>{props.player.player}</h1>
-            <h2 className={styles.headerText} id={styles.positionSeasonText}>
-              {props.player.position}, {props.player.season}
-            </h2>
-            <div>
-              <h3 className={styles.headerText} id={styles.statsHeader}>
-                <ComparablesContainer comparables={props.player.comparables} />
-              </h3>
-            </div>
-          </div>
+          <PlayerBio
+            name={props.player.player}
+            position={props.player.position}
+            season={props.player.season}
+          />
+          <ComparablesContainer comparables={props.player.comparables} />
           <TrajectoryChart
             data={[1, 2, 3, 3, 3, 3, 3, 3]}
             labels={["20", "21", "22", "23", "24", "25", "26", "27"]}
