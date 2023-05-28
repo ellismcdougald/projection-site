4082e7cc35ead2d480bf559b0f2f18e644343259
Ellis McDougald <ecmcdougald@gmail.com>
2023-05-28 16:01:09 -0600

    Adds views to player card

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -7,7 +7,10 @@ import PlayerBio from "./PlayerBio";
 
 export default function PlayerCard(props) {
   return (
-    <div id={styles.container}>
+    <div
+      id={styles.container}
+      className="w-4/5 flex flex-col items-center justify-center p-10"
+    >
       {props.player && (
         <div id={styles.cardContainer}>
           <PlayerBio
@@ -15,12 +18,24 @@ export default function PlayerCard(props) {
             position={props.player.position}
             season={props.player.season}
           />
-          <ComparablesContainer comparables={props.player.comparables} />
+          {props.view === "Projection" && (
+            <TrajectoryChart
+              data={[1, 2, 3, 3, 3, 3, 3, 3]}
+              labels={["'20", "'21", "'22", "'23", "'24", "'25", "'26", "'27"]}
+            />
+          )}
+          {props.view === "Skills Breakdown" && (
+            <SkillsChart player={props.player} />
+          )}
+          {props.view === "Comparables" && (
+            <ComparablesContainer comparables={props.player.comparables} />
+          )}
+          {/* <ComparablesContainer comparables={props.player.comparables} />
           <TrajectoryChart
             data={[1, 2, 3, 3, 3, 3, 3, 3]}
             labels={["20", "21", "22", "23", "24", "25", "26", "27"]}
           />
-          <SkillsChart player={props.player} />
+          <SkillsChart player={props.player} /> */}
         </div>
       )}
     </div>
