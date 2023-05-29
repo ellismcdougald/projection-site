c7e3ce19def23332ac586a4e0cca1d5b9c5cf8db
Ellis McDougald <ecmcdougald@gmail.com>
2023-05-28 20:20:12 -0600

    Adds styling

--- src/components/ComparableCard.js
+++ src/components/ComparableCard.js
@@ -3,53 +3,12 @@ import styles from "../styles/ComparableCard.module.css";
 import TrajectoryChart from "./TrajectoryChart";
 
 export default function ComparableCard(props) {
-  const data = {
-    labels: ["20", "21", "22", "23", "24", "25", "26", "27"],
-    datasets: [
-      {
-        labels: "Projection",
-        data: [
-          1.95, 2.46, 3.84, 3.03928268, 2.9608289127, 2.9737694739,
-          3.1355211395, 3.3008620495,
-        ],
-        backgroundColor: "black",
-        borderColor: "black",
-        pointBorderColor: "black",
-      },
-    ],
-  };
-  const options = {
-    plugins: {
-      title: {
-        display: true,
-        text: "Custom Chart Title",
-      },
-      legend: false,
-    },
-    scales: {
-      y: {
-        title: {
-          display: false,
-          text: "PTS / 60",
-        },
-        min: 0,
-        max: 6,
-      },
-      x: {
-        title: {
-          display: false,
-          text: "Season",
-        },
-      },
-    },
-  };
   return (
-    <div id={styles.container}>
-      <div>
+    <div id={styles.container} className="flex justify-between font-mono">
+      <div className="flex flex-col justify-center text-left">
         <h1 id={styles.nameText}>{props.playerArr[0]}</h1>
-        <h2 id={styles.subText}>
-          {props.playerArr[2]}, {`Similarity: ${props.playerArr[3].toFixed(2)}`}
-        </h2>
+        <h2 id={styles.subText}>{props.playerArr[2]}</h2>
+        <h3>Similarity: {props.playerArr[3].toFixed(2)}</h3>
       </div>
       <div id={styles.rightContainer}>
         <TrajectoryChart
