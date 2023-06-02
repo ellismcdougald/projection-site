82eccb1e7b524656a70eb60ce4c9a3f864f8bac7
Ellis McDougald <ecmcdougald@gmail.com>
2023-06-01 19:17:27 -0600

    Adds key

--- src/components/ComparablesContainer.js
+++ src/components/ComparablesContainer.js
@@ -1,21 +1,13 @@
 import React from "react";
 import ComparableCard from "./ComparableCard";
 
-const testArr = [
-  "Sidney Crosby",
-  "F",
-  "2013-14",
-  36.82617669,
-  [5.46, 4.42, 3.55],
-  [3.27, 3.12, 3.58, 3.15, 3.62],
-];
-
 export default function ComparablesContainer(props) {
   return (
     <div className="container m-auto grid grid-cols-2 gap-x-10 gap-y-2">
       {props.comparables.map((comp) => {
-        console.log(Object.keys(comp["last_3"]));
-        return <ComparableCard playerDict={comp} />;
+        return (
+          <ComparableCard playerDict={comp} key={comp.player + comp.season} />
+        );
       })}
     </div>
   );
